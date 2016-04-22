3598f84198d446e4b4758c6b395f2048edd65ee2
Martin Peck <mpeck@hotmail.co.uk>
2016-04-22 15:39:23 +0100

    added code comments

--- landingpage/__init__.py
+++ landingpage/__init__.py
@@ -6,5 +6,6 @@ from flask import Flask, render_template, session, redirect, url_for
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
+# I had to change this import to get your code to work
 import landingpage.views
 
--- runserver.py
+++ runserver.py
@@ -6,6 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 from landingpage import app
 
+# You don't want this. It is over writing the import above
 # app = Flask(__name__)
 
 if __name__ == '__main__':
